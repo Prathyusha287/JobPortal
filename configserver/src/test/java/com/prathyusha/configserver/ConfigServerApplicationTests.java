50761292b761abcd96f54240c8dca90ba643874b
Prathyusha <prathyushakumar7@gmail.com>
2025-03-22 15:27:23 +0530

    gateway commiting

---
+++ configserver/src/test/java/com/prathyusha/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prathyusha.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
